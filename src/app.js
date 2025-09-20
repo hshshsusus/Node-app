b242d62cdc6cb1e657ee8f6ee44fa96066e7a50c
Durgaprasad_Kasa <prasadkasa2@gmail.com>
2025-09-20 12:24:45 +0530

    middlrwares

--- src/app.js
+++ src/app.js
@@ -2,10 +2,21 @@ const express = require("express");
 const app = express();
 const port = 7777;
 
-app.use("/test",(req, res) => {
-    res.send("98745632npm s");
+app.get("/admin", (req, res,next) =>{
+   const token = "xyz";
+   const isAuth = token === "xyz";
+   if(!isAuth){
+    res.status(404).send("unautharised access!!")
+   }else{
+    next();
+   }
 })
-
+app.get("/admin/getAllData", (req, res) =>{
+    res.send("getall data")
+})
+app.get("/admin/deleteUserData", (req, res) => {
+    res.send("delete user")
+})
 app.listen(port, () =>{
     console.log(`server is running on ${port}...`)
 })
